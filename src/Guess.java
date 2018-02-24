5ea0ce60845f41070ac9ee5f0f0d5d4279e66788
astnreaperx <34556268+astnreaperx@users.noreply.github.com>
2018-02-24 15:50:55 -0600

    test

--- src/Guess.java
+++ src/Guess.java
@@ -10,11 +10,14 @@ public class Guess {
 	public static void Guess(String[] args)
 	{
 		
+	System.out.print("Guess A Random Number");
+    Scanner inputUser = new Scanner(System.in);
 	int answer = (int) Math.floor(Math.random() * 10);
 		
 	if (answer = input)
 	{
 		System.out.println("Your guess was " + input + " and that is correct!");
+		System.out.println("Your guess was " + inputUser + " and that is correct!");
 		
 	}
 	
