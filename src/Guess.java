b32955b70f53bed150c412188ae57ec6d757a5c4
zmpeg <zandstra.matt@gmail.com>
2018-02-24 17:29:01 -0700

    fix program to pass

--- src/Guess.java
+++ src/Guess.java
@@ -2,31 +2,23 @@
 import java.util.Scanner;
 import java.util.*;
 
-
 public class Guess {
 
+	public static void main(String[] args) {
 
-	
-	public static void Guess(String[] args)
-	{
-		
-	System.out.print("Guess A Random Number from 1 - 10");
+		System.out.print("Guess A Random Number from 1 - 10");
     Scanner inputUser = new Scanner(System.in);
-	
-	int answer = (int) Math.floor(Math.random() * 10);
-	
-	if(answer = inputUser) // this is not working i think it may be a converions issue
-	{
 		
-		System.out.println("Your guess was " + inputUser + " and that is correct!");
-		
-	}
+		int answer = (int) Math.floor(Math.random() * 10);
 	
-	else
-	{
-		
-		System.out.println("Wrong!");
+		if(answer == inputUser.nextInt()) // this is not working i think it may be a converions issue
+		{
+			System.out.println("Your guess was " + inputUser + " and that is correct!");
+		}
+		else
+		{
+			System.out.println("Wrong, the number was " + answer);
+		}
 	}
-	
-	}
-}//test test 
+
+}
